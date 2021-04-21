c6a91539cec92d96c75dc8d7a5d59a349c5d46a9
Teodor Wisniewski <teodorwisniewski@gmail.com>
2021-04-21 21:06:38 +0200

    replacing all dots except the last one

--- removing_forbidden_characters_from_filenames.py
+++ removing_forbidden_characters_from_filenames.py
@@ -4,11 +4,20 @@ import os
 
 script_name = os.path.basename(__file__)
 
+
+def removing_unwanted_characters(text:str) ->str:
+    flag = re.findall(r"[^a-zA-Z0-9\_]",filename)
+    if flag:
+        text =  re.sub('[^a-zA-Z0-9\.\_]', '_', text)
+        # replaces all dots except the last one
+        text = text.replace('.', '', text.count('.') - 1)
+    return text
+
+
 print(f"Starting the script {script_name}")
 for filename in glob.glob('*.*'):
-    flag = re.findall(r"[^a-zA-Z0-9\.\_]",filename)
-    if flag:
-        new_file_name =  re.sub('[^a-zA-Z0-9\.\_]', '_', filename)
+    new_file_name =  removing_unwanted_characters(filename)
+    if filename != new_file_name:
         os.rename(filename,new_file_name)
         print(f"I have just renamed a file from  called {filename} to {new_file_name}")
 
