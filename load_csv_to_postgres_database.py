52438ea574c1868bb8ee0705cee80d921e4681b9
Teodor Wisniewski <teodor.wisniewski@altmansolon.com>
2021-04-29 17:58:23 +0200

    adding cleaning and loading bar to the script

--- load_csv_to_postgres_database.py
+++ load_csv_to_postgres_database.py
@@ -6,10 +6,44 @@ import csv
 import os
 import pandas as pd
 import numpy as np
+import sys
+from tqdm import tqdm
 
 #internal imports
 from creating_sql_query_table_from_csv import turn_csv_into_sql_table
 from load_data_to_pandas_from_postgres_database import read_sql_table_to_df
+from removing_forbidden_characters_from_filenames import removing_unwanted_characters
+# TODO: Using the prepare sql statement to accelerate the sql query execution
+
+
+# define a function that handles and parses psycopg2 exceptions
+def print_psycopg2_exception(err):
+    # get details about the exception
+    err_type, err_obj, traceback = sys.exc_info()
+
+    # get the line number when exception occured
+    line_num = traceback.tb_lineno
+
+    # print the connect() error
+    print ("\npsycopg2 ERROR:", err, "on line number:", line_num)
+    print ("psycopg2 traceback:", traceback, "-- type:", err_type)
+
+    # psycopg2 extensions.Diagnostics object attribute
+    print ("\nextensions.Diagnostics:", err.diag)
+
+    # print the pgcode and pgerror exceptions
+    print ("pgerror:", err.pgerror)
+    print ("pgcode:", err.pgcode, "\n")
+
+
+def clean_row(row: list):
+
+    for index,val in enumerate(row):
+    if val == '':
+        row[index] = np.nan
+
+    return row
+
 
 
 def check_connection(conn):
@@ -18,13 +52,13 @@ def check_connection(conn):
     return f"The connection to the db is {flag_str}"
 
 
-csv_file_name = 'user_accounts.csv'
-hostname = 'localhost' #os.environ.get('HOST_POSTGRES')
-username = 'postgres' #os.environ.get('DB_USER')
-password = os.environ.get('DB_PASS_LOCAL') #os.environ.get('DB_PASS') #
-database = 'postgres'
-schema_name = 'public'
-table_name = 'users'
+csv_file_name = r'data/RFNSA AUS Tower Database.csv'
+hostname = os.environ.get('HOST_POSTGRES') #'localhost' #
+username = os.environ.get('DB_USER') #'postgres' #
+password = os.environ.get('DB_PASS') #os.environ.get('DB_PASS_LOCAL') #
+database = 'reference'
+schema_name = 'staging_asiapac'
+table_name = removing_unwanted_characters('RFNSA AUS Tower Database').lower()
 
 try:
     conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
@@ -46,15 +80,16 @@ try:
     # TODO: at python morsels tasks to improve it. And test it on
 
     # TODO: test it updated_sample_submission.csv' file, which is particularly difficult
+
     with conn as conn_populate_a_table:
         cur = conn_populate_a_table.cursor()
         with open(csv_file_name, 'r') as file:
             next(file) # skip csv header (first row with column titles)
             reader = csv.reader(file)
-            for row in reader:
+            for row_nb,row in tqdm(enumerate(reader)):
                 # insert row here
                 sub_string_s = ', '.join(['%s' for val in row])
-                cur.execute(f"INSERT INTO {schema_name}.{table_name} VALUES ({sub_string_s});", row)
+                cur.execute(f"INSERT INTO {schema_name}.{table_name} VALUES ({sub_string_s});", clean_row(row))
 
     df_from_the_db = read_sql_table_to_df(conn, schema_name, table_name)
     print(df_from_the_db.head())
@@ -63,6 +98,8 @@ try:
 except Exception as err:
     print("Oops! An exception has occured:", err)
     print("Exception TYPE:", type(err))
+    print_psycopg2_exception(err)
+
 finally:
     conn.close()
 print(check_connection(conn))
